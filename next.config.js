9482d76541a30cca9896b3634af4ef1629dc6577
Kaitlin Shumake <shumakek@ufl.edu>
2023-11-11 02:43:41 -0500

    updated next.config and tsconfig to fix deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig}  */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+    tsconfigPath: "./tsconfig.json",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
